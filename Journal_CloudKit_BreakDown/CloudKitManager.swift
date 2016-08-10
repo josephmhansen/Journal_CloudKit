ff2811b0d0cb87ce84a0e89de5409035f4c77dab
Joseph Hansen <theythemtheir@icloud.com>
2016-08-10 13:38:32 -0600

    finished working

---
+++ Journal_CloudKit_BreakDown/CloudKitManager.swift
@@ -0,0 +1,9 @@
+//
+//  CloudKitManager.swift
+//  Journal_CloudKit_BreakDown
+//
+//  Created by Joseph Hansen on 8/10/16.
+//  Copyright © 2016 home. All rights reserved.
+//
+
+import Foundation
---
+++ Journal_CloudKit_BreakDown/Entry+Cloudkit.swift
@@ -0,0 +1,9 @@
+//
+//  Entry+Cloudkit.swift
+//  Journal_CloudKit_BreakDown
+//
+//  Created by Joseph Hansen on 8/10/16.
+//  Copyright © 2016 home. All rights reserved.
+//
+
+import Foundation
